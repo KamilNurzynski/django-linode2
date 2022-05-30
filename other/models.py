26fe833055a9d8cbcbb193d50585255f5e8af6d8
Kamil Nurzynski <nurzynski.kamil@gmail.com>
2022-05-30 18:36:25 +0200

    little changes

--- other/models.py
+++ other/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Participant(models.Model):
+    first_name = models.CharField(max_length=60)
+    last_name = models.CharField(max_length=60)
+    tel_nr = models.CharField(max_length=9)
+    email = models.EmailField()
