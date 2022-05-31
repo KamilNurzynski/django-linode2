f827df25aa0154cb19472ff1b6f912302d3ac8d1
Kamil Nurzynski <nurzynski.kamil@gmail.com>
2022-05-31 14:08:41 +0200

    jakes proby

--- other/urls.py
+++ other/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
 
+
+app_name = 'other'
 urlpatterns = [
-    path('', views.simple_view)
+    path('', views.simple_view),
+    path('home/', views.HomeView.as_view(), name='home'),
+    path('thank_you/', views.ThankYouView.as_view(), name='thank_you'),
+    path('contact/', views.ContactFormView.as_view(), name='contact')
 ]
--- other/views.py
+++ other/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.views.generic import TemplateView, FormView, ListView
+from other.forms import ContactForm
 
 # Create your views here.
+
+
 def simple_view(request):
     return HttpResponse("Cześć Kamil")
+
+
+class ThankYouView(TemplateView):
+    template_name = 'other/thank_you.html'
+
+
+class HomeView(TemplateView):
+    template_name = 'other/home.html'
+
+
+class ContactFormView(FormView):
+    form_class = ContactForm
+    template_name = 'other/contact.html'
